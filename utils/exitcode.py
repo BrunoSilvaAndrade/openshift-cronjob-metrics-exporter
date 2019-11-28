3b128b66dc3df8b790e8c3d30192c06043098e8f
BrunoSilvaAndrade <brunojf.andrade@gmail.com>
2019-11-28 05:56:16 -0300

    Remove StructValidate Codes and write ExitCode Class into __init__ file

--- utils/exitcode.py
+++
@@ -1,6 +0,0 @@
-class _ExitCode:
-    exit_codes=["OK","FAIL"]
-    def __getattr__(self, name):  
-        if name in _ExitCode.exit_codes:
-            return _ExitCode.exit_codes.index(name)
-        raise AttributeError("Exit code %s not found" % name)
